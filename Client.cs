0a378ae179f8d0d94fc7e296ed93c8d8e4086be5
nielsmoller <113173868+nielsmoller@users.noreply.github.com>
2022-10-25 20:50:10 +0200

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,18 @@
+﻿using Classes;
+using System;
+
+public class Client
+{
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+	public string Adress { get; set; }
+	public BankAccount BankAccount { get; set; }
+
+	public Client(string firstName, string lastName, string adress)
+	{
+		FirstName = firstName;
+		LastName = lastName;
+		Adress = adress;
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Classes;
+
+string firstName;
+string lastName;
+string adress;
+
+
+Console.WriteLine("Hello! Welcome to the Bank. Please enter your data to setup an account.\n");
+
+Console.Write("First name: ");
+firstName = Console.ReadLine();
+Console.Write("Last name: ");
+lastName = Console.ReadLine();
+Console.Write("Adress: ");
+adress = Console.ReadLine();
+Console.Clear();
+
+Client client1 = new Client(firstName, lastName, adress);
+
+BankAccount account = new BankAccount(client1.FirstName, 1000);
+client1.BankAccount = account;
+
+
+Console.WriteLine($"Client setup with \n\nFirst name: {client1.FirstName}\nLast name: {client1.LastName}\nAdress: {client1.Adress}\n\n");
+
+
+Console.WriteLine($"Account {client1.BankAccount.Number} was created for {client1.BankAccount.Owner} with {client1.BankAccount.Balance} initial balance.\n");
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+
+Console.WriteLine(account.GetAccountHistory());
